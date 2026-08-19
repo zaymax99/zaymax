82e1fdbac870c4a95268a99250b46fa7770fe785
lolczak <blazej.doszczeczko@gmail.com>
2026-08-19 22:53:58 +0200

    Remove unused audio and video capabilities

--- app.config.ts
+++ app.config.ts
@@ -92,19 +92,6 @@ const config: ExpoConfig = {
   plugins: [
     "expo-router",
     [
-      "expo-audio",
-      {
-        microphonePermission: "Allow $(PRODUCT_NAME) to access your microphone.",
-      },
-    ],
-    [
-      "expo-video",
-      {
-        supportsBackgroundPlayback: true,
-        supportsPictureInPicture: true,
-      },
-    ],
-    [
       "expo-splash-screen",
       {
         image: "./assets/images/splash-icon.png",
