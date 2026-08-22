8e520bdad79a30ce8df5bfbf4997518cab22a1a5
lolczak <blazej.doszczeczko@gmail.com>
2026-08-22 20:02:48 +0200

    Bump version to 1.0.1

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
